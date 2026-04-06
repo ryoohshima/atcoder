070622ddb15df72bee5db05e27a0e2f34f28aa4b
Ryo-Ohshima <ryo.ohshima.official@gmail.com>
2026-04-06 22:21:42 +0900

    feat: ABC452 B問題の解答を実装

--- contests/abc/452/b/answer.js
+++ contests/abc/452/b/answer.js
@@ -1,6 +1,17 @@
 const main = (input) => {
-  const input_values = input.split('\n');
+  const input_values = input.split("\n").filter(Boolean)[0];
+  const row = Number(input_values.split(" ")[0]);
+  const column = Number(input_values.split(" ")[1]);
 
-}
+  const array = Array.from({ length: row });
 
-main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+  array.map((v, i) => {
+    if (i === 0 || i === row - 1) {
+      console.log("#".repeat(column));
+    } else {
+      console.log("#" + ".".repeat(column - 2) + "#");
+    }
+  });
+};
+
+main(require("fs").readFileSync("/dev/stdin", "utf8"));
